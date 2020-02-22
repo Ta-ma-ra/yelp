7c21fe145c14b66fe6d708ea0c7634f3087ad433
Tamara Artenstein <tara_art@live.com>
2020-02-22 11:40:41 +0200

    yelp advanced routing lecture. validations and namepaced admin included. no front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    stars:        5,
+    chef:         'Jamie Oliver'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    stars:        4,
+    chef:         'James Quebec'
+  },
+  {
+    name:         'Pizza West',
+    address:      '56A Geordie Shore, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving authentic pizza and antipasti.',
+    stars:        3,
+    chef:         'Jean Quebec'
+  },
+  {
+    name:         'Pizza North',
+    address:      'Bottom Beach, London E1 6PQ',
+    description:  'Pizzeria with classy looks, serving obtuse pizza and antipasti.',
+    stars:        5,
+    chef:         'James John'
+  }
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
